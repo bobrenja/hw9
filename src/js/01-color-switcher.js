7ac5e0f80bb6807b9b1117b7592855fc408c3a81
bobrenja <abcd43@ukr.net>
2022-11-25 11:26:08 +0200

    start

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,33 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+  span: document.querySelector('span'),
+};
+let timerId = null;
+let count = 0
+refs.start.addEventListener('click', changeColor);
+refs.stop.addEventListener('click', stopChangeColor);
+
+// console.log(refs.start);
+
+function changeColor() {
+  refs.start.setAttribute('disabled', 'disabled');
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+    refs.span.textContent = count--
+  }, 1000);
+
+  // refs.body.style.backgroundColor = getRandomHexColor()
+  //   console.log('click start', getRandomHexColor());
+  //   console.log(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function stopChangeColor() {
+  refs.start.removeAttribute('disabled');
+  clearInterval(timerId);
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,112 @@
+import flatpickr from 'flatpickr';
+import { Ukrainian } from 'flatpickr/dist/l10n/uk.js';
+// import Notify from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+require('flatpickr/dist/themes/material_green.css');
+
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+  all: document.querySelectorAll('.value'),
+};
+
+refs.start.setAttribute('disabled', 'disabled');
+
+const date = new Date();
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  locale: Ukrainian,
+
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    if (selectedDates[0] < date) {
+      refs.start.setAttribute('disabled', 'disabled');
+
+      Notify.failure('Please choose a date in the future', {
+        clickToClose: true,
+        position: 'center-top',
+      });
+      return;
+    }
+    
+    refs.start.removeAttribute('disabled');
+  },
+};
+
+const selectData = flatpickr(
+  '#datetime-picker',
+  options
+  // {
+  //   enableTime: true,
+  //   dateFormat: 'Y-m-d H:i',
+  //   locale: Ukrainian,
+  //   minDate: date,
+  //   disableMobile: 'true',
+  // }
+);
+
+refs.start.addEventListener('click', startTimer);
+let timerId = null;
+let timeFinish = null;
+
+function startTimer() {
+  refs.start.setAttribute('disabled', 'disabled');
+  timerId = setInterval(changeDay, 1000);
+
+  console.log(refs.all);
+
+  //     refs.all.forEach(e=>{
+  //         console.log(e.innerText)
+  //         e.textContent = a++
+  // })
+}
+
+function changeDay() {
+  timeFinish = selectData.selectedDates[0] - new Date();
+  const timeConvertData = convertMs(timeFinish);
+  addText(timeConvertData);
+
+  if (timeFinish < 1000) clearInterval(timerId);
+}
+
+function addText(value) {
+  refs.all.forEach(e => {
+    // console.log()
+    const keyData = Object.keys(e.dataset);
+    e.textContent = value[keyData[0]];
+  });
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
